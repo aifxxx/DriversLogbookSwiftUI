ffd5a684a391c085f11153f5560346fc38b1e53c
annefeistauer <anne.feistauer@mhp.com>
2020-06-17 11:55:05 +0200

    Initial Commit

---
+++ DriversLogbookSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DriversLogbookSwiftUI
+//
+//  Created by Anne Feistauer on 17.06.20.
+//  Copyright © 2020 Anne Feistauer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
